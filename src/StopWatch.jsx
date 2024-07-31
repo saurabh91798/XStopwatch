9471084de2fed42e32d7a3545e9251da6d53c7a5
K.Saurabh Rao <saurabh91798@gmail.com>
2024-07-31 20:16:05 +0530

    added new code for stopwatch

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -1,51 +1,48 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import {useState,useEffect,React} from "react";
 
-const Stopwatch = () => {
-  const [timer, setTimer] = useState(0);
-  const [isRunning, setisRunning] = useState(false);
-  const formatTime = (second) => {
-    const minutes = Math.floor(second /60);
-    const secs = second % 60;
-    return `${minutes}:${secs < 10 ? "0":""}${secs}`
-  }
+const StopWatch=()=>{
+    const[timerOn,setTimerOn]=useState(false)
+    const[time,setTime]=useState(0)
 
-  useEffect(() => {
-    let interval;
-
-    if (isRunning) {
-      interval = setInterval(() => {
-        setTimer((prev) => prev + 1);
-      }, 1000);
-    } else {
-      clearInterval(interval);
+    useEffect(()=>{
+      let interval=null;
+      if(timerOn){
+      interval= setInterval(()=>{
+           setTime(prev=>prev+1);
+      },1000)
+    }else{
+        clearInterval(interval)
     }
+    return()=>clearInterval(interval)
+    },[timerOn])
 
-    return () => clearInterval(interval);
-  }, [isRunning]);
-
-  return (
-    <>
-      <h1>Stopwatch</h1>
-      <h2>time : {formatTime(timer)}</h2>
-      <button
-        onClick={() => {
-          setisRunning((prev) => !prev);
-        }}
-      >
-        {isRunning ? "Stop" : "Start"}
-      </button>
-      <button
-        onClick={() => {
-          setTimer(0)
-          setisRunning(false)
-        }}
-      >
-        Reset
-      </button>
-    </>
-  );
-};
-
-export default Stopwatch;
+    const handleStart=()=>{
+        setTimerOn(true)
+    }
+    const handleStop=()=>{
+        setTimerOn(false)
+    }
+    const handleReset=()=>{
+        setTime(0)
+        setTimerOn(false)
+    }
+    const formatTime=()=>{
+        const minutes= Math.floor(time/60);
+        const seconds= time%60;
+        return `${minutes}:${seconds.toString().padStart(2, '0')}`
+    }
+    return (
+        <center>
+        <h1>Stopwatch</h1>
+        <p>Time: {formatTime()}</p>
+        {!timerOn?(
+        <button onClick={()=>handleStart()}>Start</button>
+        ):(
+            <button onClick={()=>handleStop()}>Stop</button>
+        )}
+        <button onClick={()=>handleReset()}>Reset</button>
+        
+        </center>
+    )
+}
+export default StopWatch;
